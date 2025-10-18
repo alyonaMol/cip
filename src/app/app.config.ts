64a425574ed91fb4e0e02b516a9cbb519b7587f8
voloshynajelena <voloshynajelena@gmail.com>
2025-10-18 12:00:49 -0600

    (config) prettier setup and optimizing imports

--- src/app/app.config.ts
+++ src/app/app.config.ts
@@ -1,32 +1,28 @@
-import {provideHttpClient, withInterceptors} from '@angular/common/http';
-import {ApplicationConfig} from '@angular/core';
-import {provideRouter} from '@angular/router';
-import {routes} from './app.routes';
-import {authInterceptor} from './core/auth/auth.interceptor';
-import {MessageService} from 'primeng/api';
-import {provideState, provideStore} from '@ngrx/store';
-import {reducer as userReducer} from './shared/state/user/user.reducer';
-import {providePrimeNG} from 'primeng/config';
+import { provideHttpClient, withInterceptors } from '@angular/common/http';
+import { ApplicationConfig } from '@angular/core';
+import { provideRouter } from '@angular/router';
+import { routes } from './app.routes';
+import { authInterceptor } from './core/auth/auth.interceptor';
+import { MessageService } from 'primeng/api';
+import { provideState, provideStore } from '@ngrx/store';
+import { reducer as userReducer } from './shared/state/user/user.reducer';
+import { providePrimeNG } from 'primeng/config';
 import Aura from '@primeuix/themes/aura';
 
 export const appConfig: ApplicationConfig = {
   providers: [
-  provideRouter(routes),
-  MessageService,
-  provideStore(),
-  providePrimeNG({
-    theme: {
-      preset: Aura,
-      options: {
-        darkModeSelector: false || 'none',
+    provideRouter(routes),
+    MessageService,
+    provideStore(),
+    providePrimeNG({
+      theme: {
+        preset: Aura,
+        options: {
+          darkModeSelector: false || 'none',
+        },
       },
-    }
-  }),
-  provideState({ name: 'user', reducer: userReducer }),
-    provideHttpClient(
-      withInterceptors([
-        authInterceptor,
-      ])
-    ),
+    }),
+    provideState({ name: 'user', reducer: userReducer }),
+    provideHttpClient(withInterceptors([authInterceptor])),
   ],
 };
