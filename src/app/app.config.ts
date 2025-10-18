62e23a68fd0fe4a59b5f2fa10474163c58a9702b
voloshynajelena <voloshynajelena@gmail.com>
2025-10-18 11:59:34 -0600

    (config) prime ng fix

--- src/app/app.config.ts
+++ src/app/app.config.ts
@@ -1,20 +1,27 @@
-import { provideHttpClient, withInterceptors } from '@angular/common/http';
-import { ApplicationConfig } from '@angular/core';
-import { provideRouter } from '@angular/router';
-import { routes } from './app.routes';
-import { authInterceptor } from './core/auth/auth.interceptor'; 
-import { MessageService } from 'primeng/api'; 
-import { provideAnimations } from '@angular/platform-browser/animations';
-import { provideStore } from '@ngrx/store';
-import { provideState } from '@ngrx/store';
-import { reducer as userReducer } from './shared/state/user/user.reducer';
+import {provideHttpClient, withInterceptors} from '@angular/common/http';
+import {ApplicationConfig} from '@angular/core';
+import {provideRouter} from '@angular/router';
+import {routes} from './app.routes';
+import {authInterceptor} from './core/auth/auth.interceptor';
+import {MessageService} from 'primeng/api';
+import {provideState, provideStore} from '@ngrx/store';
+import {reducer as userReducer} from './shared/state/user/user.reducer';
+import {providePrimeNG} from 'primeng/config';
+import Aura from '@primeuix/themes/aura';
 
 export const appConfig: ApplicationConfig = {
   providers: [
   provideRouter(routes),
   MessageService,
-  provideAnimations(),
   provideStore(),
+  providePrimeNG({
+    theme: {
+      preset: Aura,
+      options: {
+        darkModeSelector: false || 'none',
+      },
+    }
+  }),
   provideState({ name: 'user', reducer: userReducer }),
     provideHttpClient(
       withInterceptors([
